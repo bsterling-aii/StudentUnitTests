695d2983e3a3f435de0e466ede83474c56ac521c
AIV\bsterling <bsterling@aii.edu.com>
2016-10-18 20:50:12 -0700

    test commit

---
+++ StudentUnitTest/Main.cpp
@@ -0,0 +1,19 @@
+#include <cppunit/XmlOutputter.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+
+int main(int argc, char* argv[])
+{
+	CPPUNIT_NS::Test *suite = CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest();
+	CppUnit::TextUi::TestRunner runner;
+	//std::ofstream fs("Result.xml");
+	//CppUnit::XmlOutputter* outputter = new CppUnit::XmlOutputter(&runner.result(), fs);
+	//runner.setOutputter(outputter);
+
+	runner.addTest(suite);
+	bool wasSucessful = runner.run();
+
+	return wasSucessful ? 0 : 1;
+}
